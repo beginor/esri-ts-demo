b25c96c05c9830ab3a6a3b469d4a76fadc3307b2
beginor <beginor@qq.com>
2020-09-04 09:48:28 +0800

    add esri map view demo.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,41 @@
-export class App {
+import { loadScript, loadModules } from 'esri-loader';
+export class MapApp {
 
-    constructor() { }
-
-    public run(): void {
-        console.log('Hello, Rollup!');
+    private mapView: __esri.MapView;
+  
+    /** Load ArcGIS JS API scripts. */
+    async loadScript() {
+      const AGS_SDK = 'https://js.arcgis.com/4.16';
+      await loadScript({
+        url: `${AGS_SDK}/init.js`,
+        css: `${AGS_SDK}/esri/themes/light/main.css`
+      });
     }
-
-}
+  
+    /** Init a map view */
+    async initMap(container) {
+      const [Map, MapView] = await loadModules<[__esri.MapConstructor, __esri.MapViewConstructor]>(['esri/Map', 'esri/views/MapView']);
+      const map = new Map({ basemap: 'satellite' });
+      this.mapView = new MapView({
+        map, container,
+        extent: {
+          // autocasts as new Extent()
+          xmin: -9177811,
+          ymin: 4247000,
+          xmax: -9176791,
+          ymax: 4247784,
+          spatialReference: { wkid: 102100 }
+        }
+      });
+    }
+  
+    /** add a feature layer to map */
+    async addFeatureLayer() {
+      const [FeatureLayer] = await loadModules(['esri/layers/FeatureLayer']);
+      const layer = new FeatureLayer({
+        url: 'https://services.arcgis.com/V6ZHFr6zdgNZuVG0/arcgis/rest/services/Landscape_Trees/FeatureServer/0'
+      });
+      this.mapView.map.layers.add(layer);
+    }
+  
+  }
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,21 @@
-import { App } from './app/app'
+import { MapApp } from './app/app'
 
 import './main.css';
 
-var app = new App();
-app.run();
+const pathBase = `${location.protocol}//${location.host}`;
+window['dojoConfig'] = {
+  async: true,
+  packages: [
+    // { name: 'THREE', location: 'https://unpkg.com/three@0.117.1/build', main: 'three.min' },
+  ]
+};
+window['esriConfig'] = { locale: 'zh-cn' };
+
+async function initMap() {
+  const app = new MapApp();
+  await app.loadScript();
+  await app.initMap('viewDiv');
+  await app.addFeatureLayer();
+}
+
+initMap();
